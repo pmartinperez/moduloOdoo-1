a7170767c8fd47726ef6643ccf5a1c33532d46f9
damain <damian@danielcastelao.org>
2016-03-15 19:19:17 +0100

    Campos Relacionados

--- openacademy/models.py
+++ openacademy/models.py
@@ -8,6 +8,9 @@ class Course(models.Model):
     name = fields.Char(string="Titulo", required=True)
     description = fields.Text()
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsable", index=True)
+
 class Session(models.Model):
     _name = 'openacademy.session'
 
@@ -15,3 +18,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Curso", required=True)
