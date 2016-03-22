08cac2b1ab8ad5dfb46d5cd8217061253259c4f1
Patri <pmartinperez@danielcastelao.org>
2016-03-22 16:50:19 +0100

    Se han creado dos modelos Cursos y Sesiones. Se ha definido el menu para acceder a los modelos. Las vistas de los cursos y sesiones, esto es, como se muestran. Se ha permitido buscar los cursos por nombre y descripcion. Por ultimo, se ha definido la relacion entre los modelos.

--- openacademy/models.py
+++ openacademy/models.py
@@ -23,3 +23,4 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Curso", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
